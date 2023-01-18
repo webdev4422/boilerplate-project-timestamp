31ac814135ff7dd86dfe7866be19b786abda496c
user <user@gmail.com>
2023-01-18 15:41:31 +0200

    Finish code

--- index.js
+++ index.js
@@ -2,31 +2,63 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+const express = require('express')
+const app = express()
+const port = 443 //3000
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+const cors = require('cors')
+app.use(cors({ optionsSuccessStatus: 200 })) // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html')
+})
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+// TODO:
+// GET: /api/1451001600000 should return {unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT"}
+// GET: /api/2015-12-25 should return {unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT"}
+// Source: Using Route parameters https://expressjs.com/en/guide/routing.html
 
+
+// first API endpoint
+app.get( '/api/', function (req, res) {
+  let dateUtc = new Date().toUTCString()
+  res.json({ unix: Date.now(), utc: dateUtc })
+})
+
+// second API endpoint
+app.get( '/api/:date?', function (req, res) {
+  let reqParams = req.params.date
+  const regexCheckUnix = /^\d{13}$/
+  const regexCheckUtc = /^\d{4}-\d{2}-\d{2}$/
+
+  // Check if request match regex
+  if (reqParams.match(regexCheckUnix)) {
+    let convertUnixToUtc = (new Date (Number(reqParams))).toUTCString() // Tue, 12 May 2020 23:50:21 GMT
+    let paramToNumber = Number(reqParams) // typeof 1451001600000
+    res.json({unix: paramToNumber, utc: convertUnixToUtc})
+    // console.log(`Request params: ${paramToNumber}\nResponse: 'unix: ${reqParams}, utc: ${convertUnixToUtc}'`)
+
+  } else if (reqParams.match(regexCheckUtc)) {
+    let convertDateToUnix = new Date (reqParams).getTime()
+    let convertDateToUnixToUtc = (new Date (Number(convertDateToUnix))).toUTCString()
+    res.json({unix: convertDateToUnix, utc: convertDateToUnixToUtc})
+    // console.log(`Request params: ${reqParams}\nResponse: 'unix: ${convertDateToUnix}, utc: ${convertDateToUnixToUtc}'`)
+
+  } else {
+    res.json({error : "Invalid Date"})
+    // console.log(`Request params: ${reqParams}\nResponse: 'error : "Invalid Date"'`)
+  }
+})
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+const listener = app.listen(port, process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
